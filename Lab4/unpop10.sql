6735ecedae2cdf3ba4559bca12468a4b80a49f54
MaximIkonnikov <denimqa@gmail.com>
2015-11-24 00:16:13 +0300

    NEW LAB 4 REPORT

--- Lab4/unpop10.sql
+++ Lab4/unpop10.sql
@@ -1,7 +1,12 @@
-connect 'C:\SCLUB' user 'SYSDBA' password 'masterkey';
-create view unpopular10sections as select first 10 SECTIONS.sectionname as Name,
-    COUNT(SEASONTICKETS.ID_TICKET) as Tickets from SECTIONS, SEASONTICKETS
-   ,tickettypes, tickettypestosections where SEASONTICKETS.id_type = TICKETTYPES.id_type
-   and  SECTIONS.ID_SECTION in (select TICKETTYPESTOSECTIONS.id_section from TICKETTYPESTOSECTIONS
-   where tickettypestosections.id_type = tickettypes.id_type) group by SECTIONS.sectionname order by Tickets asc;
+connect 'C:/SCLUB' user 'SYSDBA' password 'masterkey';
+create view unpopular10sectionsver2 as
+select
+first 10 sectionname as Name,
+count (id_ticket) as Tickets
+from SECTIONS s
+ join tickettypestosections t on s.id_section = t.id_section
+ join tickettypes tt on tt.id_type = t.id_type
+ join seasontickets ss on ss.id_type = tt.id_type
+group by sectionname order by Tickets asc
+                    
 
